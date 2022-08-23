7bcce7120953b7b5fc076fbe231fcdceb7ffc4c2
Laura <lauraalbabernal07@gmail.com>
2022-08-23 16:02:04 -0500

    9

--- conexion.php
+++ conexion.php
@@ -3,8 +3,7 @@ $servidor="localhost";
 $usuario="root";
 $password="";
 $bd="sistema control de activides";
-$conecta= mysqli_connect($servidor,$usuario,$password,$bd);
-mysqli_set_charset($conecta,'utf8mb4_unicode_ci');
+$conecta = mysqli_connect($servidor,$usuario,$password,$bd);
 if($conecta->connect_error){
   die("Error al conectar con la base de datos".$conecta->connect_error);
 }
