3160c9fc2e38e45fe2130fd3676a2826f8bbcc18
Erick Herrera <sistemas14@red5g.co>
2022-09-07 01:22:54 -0500

    add validations

--- View/inicio/login.php
+++ View/inicio/login.php
@@ -3,8 +3,8 @@
 		<form class="form" method="post" action="?controlador=inicio&accion=validar">
 			<h2 class="form_title">Inicio De Sesión</h2>
 			<div class="login-form">
-				<input type="text" id="usr" placeholder="NºDocumento" />
-				<input type="password" id="pwd"placeholder="Contraseña" />
+				<input type="text" id="usr" placeholder="NºDocumento" required/>
+				<input type="password" id="pwd"placeholder="Contraseña" required/>
 				<input type="submit" id="submit" value="Ingresar" />   
 			</div>
 		</form>
