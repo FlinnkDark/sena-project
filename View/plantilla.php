aaaa069a462c6297c038e01558578f775a653d77
Reyes-coder <reyespiraligua@gmail.com>
2022-12-05 14:43:52 -0500

    front

--- View/plantilla.php
+++ View/plantilla.php
@@ -6,15 +6,27 @@
      <meta name="viewport" content="width=device-width, initial-scale=1">
      <link rel="shortcut icon" href="img/favicon-16x16.png" type="image/x-icon">
      <link href="Resources/css/estilos.css" rel="stylesheet">
+       <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="resources/css/bootstrap.rtl.min.css" integrity="sha384-DOXMLfHhQkvFFp+rWTZwVlPVqdIhpDVYT9csOnHSgWQWPX0v5MCGtjCJbY6ERspU" crossorigin="anonymous">
 </head>
 <body>
   <header>
-      <section class="textosheader">
-          <h1>SISTEMA CONTROL DE ACTIVIDADES</h1>
-          <H2>Mercaderistas</H2>
-      </section>
-      <div class="wave" style="height: 150px; overflow: hidden;" ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: #8f8f19;"></path></svg></div>
-  </header>
+  <nav class="navbar navbar-expand-lg" style=" background-color: #8cc751;"> 
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#" style=" background-color: #8cc751; font-size: 25px; font-weight: bold;">Sistema de Control de Actividades</a>
+    
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#"style=" background-color: #8cc751; font-size: 20px;font-weight: bold;">Mercaderistas</a>
+        </li>
+        
+      </ul>
+      
+    </div>
+  </div>
+</nav>
+      </header>
   <?php if (isset($_SESSION["usuario_id"])) { ?>
   <div>    
     <div>
@@ -31,6 +43,8 @@
   <?php }
   require_once "rutas.php";
   ?>
+ <!-- Option 1: Bootstrap Bundle with Popper -->
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
 </body>
 </html>
